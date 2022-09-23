b198f52c5342dee028614564cdbe90f30fdbd343
ImagineGoogle <113044328+ImagineGoogle@users.noreply.github.com>
2022-09-23 16:30:03 -0400

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -535,6 +535,21 @@ task.spawn(function() --timer
     end)
 end)
 
+GuiObjects.TabList:GetPropertyChangedSignal("Visible"):Connect(function()
+    if GuiObjects.TabList.Visible == false then
+        for i, v in pairs(GuiObjects.BedWarsUI.TabList:GetChildren()) do
+            if v:IsA("Frame") then
+                v:Destroy()
+            end
+        end
+        for i, v in ipairs(TeamsService:GetTeams()) do
+            for i2, v2 in pairs(v:GetPlayers()) do
+                addPlayer(v2)
+            end
+        end
+    end
+end)
+
 game:GetService("Players").PlayerAdded:Connect(function(player)
     for i, v in pairs(GuiObjects.BedWarsUI.TabList:GetChildren()) do
         if v:IsA("Frame") then
